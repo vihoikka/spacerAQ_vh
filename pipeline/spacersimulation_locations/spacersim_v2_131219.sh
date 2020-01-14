9e2582b743c57f6c255a682ee5f6b3a39d18e70d
Ville Hoikkala <ville.hoikkala@gmail.com>
2020-01-14 12:56:51 +0200

    Update spacersim_v2_131219.sh

--- pipeline/spacersimulation_locations/spacersim_v2_131219.sh
+++ pipeline/spacersimulation_locations/spacersim_v2_131219.sh
@@ -8,33 +8,33 @@ c1_pool_cluster=Pooled_spacers/C1_phage_pool_clustered.fastq #Actual C1 spacers 
 
 for i in {1..1000}
 do
-python spacersimulation_v2/random_spacer_sampling_fcl2_v2.py --genome fcl2.fasta --mode random -c 430 -ps NNNNNTAAA -pp downstream > /dev/null 2>&1
-cd-hit-est-2d -i $c1_pool_cluster -i2 randomizedSpacers.fasta -o spacersimulation_v2/clust.clstr -c 0.90 -M 16000 -T 8 > /dev/null 2>&1
+python spacersimulation_locations/random_spacer_sampling_fcl2_v2.py --genome fcl2.fasta --mode random -c 430 -ps NNNNNTAAA -pp downstream > /dev/null 2>&1
+cd-hit-est-2d -i $c1_pool_cluster -i2 randomizedSpacers.fasta -o spacersimulation_locations/clust.clstr -c 0.90 -M 16000 -T 8 > /dev/null 2>&1
 #cd-hit-est-2d -i ../SAM/CorrectedSpacers/B+P_b_+1_C1_mapped_fcl2_C1.fasta -i2 randomizedSpacers.fasta -o clust.clstr -c 0.8 -M 16000 -T 8 > /dev/null 2>&1
-clusters=$(grep ">" -c spacersimulation_v2/clust.clstr)
+clusters=$(grep ">" -c spacersimulation_locations/clust.clstr)
 #echo $clusters
 clusteredList[$cycleCounter]=$clusters
 
 cycleCounter=$((++cycleCounter))
 done
 
-printf "${clusteredList[*]}\n" > spacersimulation_v2/random_set.txt
+printf "${clusteredList[*]}\n" > spacersimulation_locations/random_set.txt
 
 #Same run for PAM version of the analysis
 cycleCounter=0
 
 for i in {1..1000}
 do
-python spacersimulation_v2/random_spacer_sampling_fcl2_v2.py --genome fcl2.fasta --mode PAM -c 430 -ps NNNNNTAAA -pp downstream > /dev/null 2>&1
-cd-hit-est-2d -i $c1_pool_cluster -i2 randomizedSpacers.fasta -o spacersimulation_v2/clust.clstr -c 0.90 -M 16000 -T 8 > /dev/null 2>&1
+python spacersimulation_locations/random_spacer_sampling_fcl2_v2.py --genome fcl2.fasta --mode PAM -c 430 -ps NNNNNTAAA -pp downstream > /dev/null 2>&1
+cd-hit-est-2d -i $c1_pool_cluster -i2 randomizedSpacers.fasta -o spacersimulation_locations/clust.clstr -c 0.90 -M 16000 -T 8 > /dev/null 2>&1
 #cd-hit-est-2d -i ../SAM/CorrectedSpacers/B+P_b_+1_C1_mapped_fcl2_C1.fasta -i2 randomizedSpacers.fasta -o clust.clstr -c 0.8 -M 16000 -T 8 > /dev/null 2>&1
-clusters=$(grep ">" -c spacersimulation_v2/clust.clstr)
+clusters=$(grep ">" -c spacersimulation_locations/clust.clstr)
 #echo $clusters
 clusteredList[$cycleCounter]=$clusters
 
 cycleCounter=$((++cycleCounter))
 done
 
-printf "${clusteredList[*]}\n" > spacersimulation_v2/PAM_set.txt
+printf "${clusteredList[*]}\n" > spacersimulation_locations/PAM_set.txt
 
 #cd-hit-est-2d -i ../Commonspacers/B+P_b_+1_C1_mapped_fcl2_C1.fastq -i2 100percent_testset.fasta -o clust.clstr -c 1.0 -M 16000 -T 8
